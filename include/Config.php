d2cd514ec0842c11755bf6cebacaa90d7c4cb4f9
jotish <jotish@ymail.com>
2017-04-09 12:02:54 +0530

    added prod config

--- include/Config.php
+++ include/Config.php
@@ -3,10 +3,19 @@
 /**
  * Database config variables
  */
+$mode = 'PROD';
 
-define("DB_HOST", "localhost");
-define("DB_USER", "id1123595_migrat");
-define("DB_PASSWORD", "indira123");
-define("DB_DATABASE", "id1123595_usedbooks");
+if ($mode == 'PROD') {
+	define("DB_HOST", "localhost");
+	define("DB_USER", "indira15_migrate");
+	define("DB_PASSWORD", "indira15");
+	define("DB_DATABASE", "indira15_books");
+} else {
+	define("DB_HOST", "localhost");
+	define("DB_USER", "id1123595_migrat");
+	define("DB_PASSWORD", "indira123");
+	define("DB_DATABASE", "id1123595_usedbooks");
+}
+
 
 ?>
